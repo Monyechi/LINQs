d98b60feb1be91b33e22207b36983df53a21a3b8
Monyechi <63468925+Monyechi@users.noreply.github.com>
2020-07-01 19:53:17 -0400

    finished question 1

---
+++ LINQ/Run.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Run
+    {
+
+       public Run()
+        {
+            Console.ReadLine();
+        }
+
+
+        public void Question1()
+        {
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+
+            var th = words.Where(w => w.Contains("th"));
+            var wordsWithTh = th.ToArray();
+
+            for (int i = 0; i < wordsWithTh.Count(); i++)
+            {
+                Console.WriteLine(wordsWithTh[i]);
+
+            }
+
+        }
+        public void Question2()
+        {
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+
+
+
+        }
+
+       
+    }
+
+    
+}
