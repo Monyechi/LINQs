a297058e4af34cef1a09785a6c05f0936afe3195
Monyechi <63468925+Monyechi@users.noreply.github.com>
2020-07-02 11:10:49 -0400

    finished question 4

--- LINQ/Run.cs
+++ LINQ/Run.cs
@@ -11,8 +11,7 @@ namespace LINQ
 
        public Run()
         {
-            Question2();
-            Console.ReadLine();
+            
         }
 
 
@@ -40,6 +39,32 @@ namespace LINQ
             {
                 Console.WriteLine(namelist[i]);
             }
+        }
+        public void Question3()
+        {
+            List<string> classGrades = new List<string>()
+            {
+                    "80,100,92,89,65",
+                    "93,81,78,84,69",
+                    "73,88,83,99,64",
+                    "98,100,66,74,55"
+            };
+
+            //var grade1 = classGrades[];
+
+        }
+        public static void Question4()
+        {
+            var text = "Terrill";
+
+            var dict = new Dictionary<char, int>();
+
+            foreach (var c in text)
+                if (dict.ContainsKey(c))
+                    dict[c]++;
+            
+
+
         }
 
        
